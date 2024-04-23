9adc36c3b898dc0b852a616f13724ad6fb185a82
Bjørn Lindholm <14328774+BjornDCode@users.noreply.github.com>
2024-04-23 11:41:19 +0200

    22.6.5 example

---
+++ 22.6.5/main.js
@@ -0,0 +1,16 @@
+import puppeteer from 'puppeteer';
+
+(async () =>{
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  await page.goto('https://example.com/')
+  await page.click('a')
+  await page.goBack()
+
+  const entry = await page.evaluate(() => performance.getEntries()[0].toJSON())
+
+  await browser.close()
+
+  console.log(entry.type) // 'navigation'
+})()
